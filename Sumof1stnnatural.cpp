f771ff2f96e651eeced951f3e0c2df354b15f9b7
SHREY247 <108192833+SHREY247@users.noreply.github.com>
2022-06-28 16:42:54 +0530

    Add files via upload

---
+++ Sumof1stnnatural.cpp
@@ -0,0 +1,21 @@
+//Print first n natural numbers
+#include<iostream>
+using namespace std;
+int main()
+{   
+	int i=1,N,sum=0;
+	cout<<"Enter the value of N\n";
+	cin>>N;
+	if(N<0)
+	cout<<"Please enter a natural number\n";
+	while(i<=N)
+	{   sum+=i;
+		cout<<i<<" ";
+		i++;
+		
+	}
+	
+	cout<<endl<<"Sum of first "<<N<<" natural numbers is "<<sum<<endl;
+	
+	return 0;
+}
